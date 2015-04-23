17b0adfdf93934c986fa132c1c660488bf34f13a
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-04-23 16:54:13 +0900

    myid

--- quorumStart.sh
+++ quorumStart.sh
@@ -2,5 +2,12 @@
 
 for i in `seq 1 3`;
 do
+    DIR=/tmp/zookeeper$i/
+    if [ ! -d $DIR ];
+    then
+	mkdir $DIR
+	echo $i > $DIR/myid
+    fi
+
     ZOO_LOG_DIR=logs/$i/ bin/zkServer.sh --config quorumconf/$i start
 done
