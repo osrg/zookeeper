6b598d2a42a7d18e259859dab3f60b2997808051
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-04-28 17:34:12 +0900

    my client

---
+++ myZkCli/concurrentWrite.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+export ZOOBINDIR=../bin/
+. ../bin/zkEnv.sh
+
+EQ_ENV_PROCESS_ID=zkcli1 EQ_MODE_DIRECT=1 java -cp $CLASSPATH:out/production/myZkCli -javaagent:/home/mitake/github/earthquake.git/inspector/java/base/target/earthquake-inspector.jar=script:hello.btm  MyZkCli localhost:2181 &
+
+EQ_ENV_PROCESS_ID=zkcli2 EQ_MODE_DIRECT=1 java -cp $CLASSPATH:out/production/myZkCli -javaagent:/home/mitake/github/earthquake.git/inspector/java/base/target/earthquake-inspector.jar=script:hello.btm  MyZkCli localhost:2182
+
