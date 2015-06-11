fabe177b9f3c4386953eb062d47fe7a7fc86a189
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-06-11 18:49:12 +0900

    update for unified inspector

--- myZkCli/concurrentWrite.sh
+++ myZkCli/concurrentWrite.sh
@@ -3,7 +3,7 @@
 export ZOOBINDIR=../bin/
 . ../bin/zkEnv.sh
 
-AGENT_CP=/home/mitake/github/earthquake.git/bin/earthquake-inspector-classic.jar
+AGENT_CP=/home/mitake/github/earthquake.git/bin/earthquake-inspector.jar
 
 EQ_ENV_PROCESS_ID=zkcli1 EQ_MODE_DIRECT=1 java -cp $CLASSPATH:out/production/myZkCli -javaagent:$AGENT_CP=script:client.btm  MyZkCli localhost:2181 &
 
